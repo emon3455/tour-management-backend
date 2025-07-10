12eb756caff5e2e97657b7a246ae45dfc8645438
emon3455 <emon.mhk69@gmail.com>
2025-07-10 11:14:37 +0600

    not found route handler addded

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import { router } from "./app/routes";
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
 
 const app = express();
 
@@ -17,5 +18,7 @@ app.get("/", (req: Request, res: Response) => {
 })
 
 app.use(globalErrorHandler)
+
+app.use(notFound)
 
 export default app;
